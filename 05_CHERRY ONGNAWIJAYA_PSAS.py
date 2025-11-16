9118fa3e753f0dd9be2f02d09e226fdccee04632
cherryy-ong <2015070116@student.pppkpetra.sch.id>
2025-11-17 06:41:12 +0700

    05_CHERRY ONGNAWIJAYA_PSAS.py

--- 05_CHERRY ONGNAWIJAYA_PSAS.py
+++ 05_CHERRY ONGNAWIJAYA_PSAS.py
@@ -11,7 +11,7 @@ def tampilkan(frame):
 def hal1_login():
     username = str(entry.get())
     password = str(entry2.get())
-    if password == "1" and username == "c":
+    if password == "12345" and username == "cherryy.ong":
         tampilkan(hal2)
     elif password == "ad123" and username == "admin":
         tampilkan(hal6)
@@ -19,11 +19,11 @@ def hal1_login():
         label_hasil.config(text=f"Password atau Username salah!", font=("Times New Roman", 10, "bold", "italic"), fg="red")
 
 def hal2_simpandata():
-    nama = str(entry3.get())
+    email = str(entry3.get())
     film = str(select.get())
     pembayaran = str(status.get())
 
-    if not nama or film == "Select a film" or not pembayaran:
+    if not email or film == "Select a film" or not pembayaran:
         label_hasil2.config(text="Semua data harus diisi dengan benar!", font=("Times New Roman", 10, "bold", "italic"), fg="red")
         return
     
@@ -34,7 +34,7 @@ def hal2_simpandata():
         label_hasil2.config(text="Semua data harus diisi dengan benar!", font=("Times New Roman", 10, "bold", "italic"), fg="red")
         return
 
-    data.append({"Nama": nama, "Film": film, "Jumlah Tiket": jumlah_tiket, "Harga": harga, "Pembayaran": pembayaran})
+    data.append({"Email": email, "Film": film, "Jumlah Tiket": jumlah_tiket, "Harga": harga, "Pembayaran": pembayaran})
     label_hasil2.config(text="Data berhasil disimpan!", font=("Times New Roman", 10, "bold", "italic"), fg="green")
 
     entry3.delete(0, tk.END)
@@ -51,7 +51,7 @@ def hal3_tampilkandata():
 
     if data:
         for item in data:
-            tabel.insert("", tk.END, values=(item["Nama"], item["Film"], item["Jumlah Tiket"], item["Harga"], item["Pembayaran"]))
+            tabel.insert("", tk.END, values=(item["Email"], item["Film"], item["Jumlah Tiket"], item["Harga"], item["Pembayaran"]))
     else:
         tabel.insert("", tk.END, values=("Belum ada data", "-", "-", "-", "-"))
 
@@ -63,11 +63,11 @@ def hal4_caridata():
     for i in tabel_cari.get_children():
         tabel_cari.delete(i)
 
-    hasil = [item for item in data if keyword in item["Nama"].lower()]
+    hasil = [item for item in data if keyword in item["Email"].lower()]
 
     if hasil:
         for item in hasil:
-            tabel_cari.insert("", tk.END, values=(item["Nama"], item["Film"], item["Jumlah Tiket"], item["Harga"], item["Pembayaran"]))
+            tabel_cari.insert("", tk.END, values=(item["Email"], item["Film"], item["Jumlah Tiket"], item["Harga"], item["Pembayaran"]))
     else:
         tabel_cari.insert("", tk.END, values=("Data tidak ditemukan", "-", "-", "-", "-"))
 
@@ -95,8 +95,6 @@ def hal5_laporandata():
 
 def hal6_tambahdata():
     addon = entry11.get()
-
-    label_hasil6.config(text="")
     
     if addon == "":
         label_hasil6.config(text="Masukkan Judul Film yang Belum Ada!", font=("Times New Roman", 10, "bold", "italic"), fg="red")
@@ -118,8 +116,6 @@ def hal6_tambahdata():
 
 def hal7_hapusdata():
     rev = entry12.get()
-
-    label_hasil7.config(text="")
 
     if rev == "":
         label_hasil7.config(text="Masukkan Judul Film yang Sudah Ada!", font=("Times New Roman", 10, "bold", "italic"), fg="red")
@@ -206,14 +202,14 @@ button8.pack()
 hal4 = Frame(jendela, bg="#BDD5E3")
 label9 = Label(hal4, text="( Cari Data Pemesanan Film Bioskop )", font=("Georgia", 20, "bold"), fg="black", bg="#BDD5E3")
 label9.pack(pady=5)
-label10 = Label(hal4, text="Masukkan Nama: ", font=("Times New Roman", 10, "bold"), bg="#BDD5E3")
+label10 = Label(hal4, text="Masukkan Email: ", font=("Times New Roman", 10, "bold"), bg="#BDD5E3")
 label10.pack(pady=5)
 entry10 = Entry(hal4)
 entry10.pack()
 button6 = Button(hal4, text="Cari Data", command=hal4_caridata, bg="#FFFFFF")
 button6.pack()
 
-kolom_cari = ("Nama", "Film", "Jumlah Tiket", "Harga", "Pembayaran")
+kolom_cari = ("Email", "Film", "Jumlah Tiket", "Harga", "Pembayaran")
 tabel_cari = ttk.Treeview(hal4, columns=kolom_cari, show="headings", height=10)
 for kol in kolom_cari:
     tabel_cari.heading(kol, text=kol)
@@ -227,7 +223,7 @@ hal3 = Frame(jendela, bg="#BDD5E3")
 label8 = Label(hal3, text="( Data Pemesanan Film Bioskop )", font=("Georgia", 20, "bold"), fg="black", bg="#BDD5E3")
 label8.pack(pady=5)
 
-kolom = ("Nama", "Film", "Jumlah Tiket", "Harga", "Pembayaran")
+kolom = ("Email", "Film", "Jumlah Tiket", "Harga", "Pembayaran")
 tabel = ttk.Treeview(hal3, columns=kolom, show="headings", height=10)
 for kol in kolom:
     tabel.heading(kol, text=kol)
@@ -240,7 +236,7 @@ button5.pack()
 hal2 = tk.Frame(jendela, bg="#BDD5E3")
 judul2 = Label(hal2, text="( Input Data )", font=("Georgia", 20, "bold"), bg="#BDD5E3", fg="black")
 judul2.pack(pady=5)
-label3 = Label(hal2, text=f"Nama: ", font=("Times New Roman", 10, "bold"), bg="#BDD5E3")
+label3 = Label(hal2, text=f"Email: ", font=("Times New Roman", 10, "bold"), bg="#BDD5E3")
 label3.pack()
 entry3 = Entry(hal2)
 entry3.pack()
